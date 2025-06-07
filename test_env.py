0728827004161669ebd945216f43a9b3c3b25f1b
Samhitha Medulla <sxm12571@ucmo.edu>
2025-06-07 17:27:36 -0500

    Delete test_env.py

--- test_env.py
+++
@@ -1,9 +0,0 @@
-import os
-
-print("EVENT_HUB_CONN_STR =", repr(os.getenv("EVENT_HUB_CONN_STR")))
-print("EVENT_HUB_NAME =", repr(os.getenv("EVENT_HUB_NAME")))
-
-print("🚩 Printing all environment variables:")
-for k, v in os.environ.items():
-    print(f"{k}={v}")
-
